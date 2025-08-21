7fa953d7a1d31b14597493807cfa20a069ef55b8
msagr <mehulsagrawal33@gmail.com>
2025-08-21 16:39:41 +0530

    chore: get data in tRPC server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { caller } from "@/trpc/server";
 
 export default async function Page() {
+  const data  = await caller.hello({ text: "SERVER CALL"});
+
   return (
-    <div className="">
-      <Button />
+    <div>
+      {JSON.stringify(data)}
     </div>
   );
 }
