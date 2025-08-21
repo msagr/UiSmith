68eb407c202e99a8c810b7c386845c2c6941ec75
msagr <mehulsagrawal33@gmail.com>
2025-08-21 11:03:25 +0530

    chore: trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import  prisma  from "@/lib/db";
 
 export default async function Page() {
-  const users = await prisma.post.findMany();
   return (
     <div className="">
-      {JSON.stringify(users, null, 2)}
+      <Button />
     </div>
   );
 }
