dfe208501d4d7b7b2dad9fca63edf00c4ecc81dd
msagr <mehulsagrawal33@gmail.com>
2025-09-11 10:10:43 +0530

    prefetch project and messages belonging to project via id

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable */
 
+import { getQueryClient, trpc } from "@/trpc/server";
+
 interface Props {
     params: Promise<{
         projectId: string;
@@ -8,6 +10,15 @@ interface Props {
 
 const Page = async ({ params }: Props) => {
     const { projectId } = await params;
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({
+        projectId
+    }));
+    void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({
+        id: projectId,
+    }));
+
     return (
         <div>
             Project Id: {projectId}
