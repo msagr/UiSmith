a9b50a0e751c1ab39f8ed9ec6c3483b290f8f19b
msagr <mehulsagrawal33@gmail.com>
2025-09-12 16:21:51 +0530

    feat: enable fragment view functionality

---
+++ src/modules/projects/ui/components/fragment-web.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { ExternalLinkIcon, RefreshCcwIcon } from "lucide-react";
+import { Fragment } from "@/generated/prisma";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+    data: Fragment;
+};
+
+export function FragmentWeb({ data }: Props) {
+    const [fragmentKey, setFragmentKey] = useState(0);
+    const [copied, setCopied] = useState(false);
+
+    const onRefresh = () => {
+        setFragmentKey((prev) => prev + 1);
+    };
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(data.sandboxUrl);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+    };
+
+    return (
+        <div className="flex flex-col w-full h-full">
+            <div className="p-2 border-b bg-sidebar flex items-center gap-x-2">
+                <Button size="sm" variant="outline" onClick={onRefresh}>
+                    <RefreshCcwIcon />
+                </Button>
+                <Button size="sm" variant="outline" onClick={handleCopy} disabled={!data.sandboxUrl || copied} className="flex-1 justify-start text-start font-normal">
+                    <span className = "truncate">
+                        {data.sandboxUrl}
+                    </span>
+                </Button>
+                <Button
+                    size="sm"
+                    disabled={!data.sandboxUrl}
+                    variant="outline"
+                    onClick={() => {
+                        if(!data.sandboxUrl) return;
+                        window.open(data.sandboxUrl, "_blank");
+                    }}
+                >
+                    <ExternalLinkIcon />
+                </Button>
+            </div>
+            <iframe 
+                key={fragmentKey}
+                className="h-full w-full"
+                sandbox="allow-forms allow-scripts allow-same-origin"
+                loading="lazy"
+                src={data.sandboxUrl}
+            />
+        </div>
+    )
+}
