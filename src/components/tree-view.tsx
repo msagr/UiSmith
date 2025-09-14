7a76bf4d33e366b540b90d04c547591bc394957e
msagr <mehulsagrawal33@gmail.com>
2025-09-14 09:43:46 +0530

    feat: enable automatic selection of last message fragment

--- src/components/tree-view.tsx
+++ src/components/tree-view.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { TreeItem } from "@/types";
 import {
     Collapsible,
--- src/modules/projects/ui/components/message-container.tsx
+++ src/modules/projects/ui/components/message-container.tsx
@@ -17,6 +17,7 @@ interface Props {
 export const MessagesContainer = ({ projectId, activeFragment, setActiveFragment }: Props) => {
     const trpc = useTRPC();
     const bottomRef = useRef<HTMLDivElement>(null);
+    const lastAssistantMessageIdRef = useRef<string | null>(null);
 
     const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
         projectId: projectId,
@@ -25,15 +26,16 @@ export const MessagesContainer = ({ projectId, activeFragment, setActiveFragment
     }));
 
     // TODO: Causing Problem
-    // useEffect(() => {
-    //     const lastAssistantMessageWithFragment = messages.findLast(
-    //         (message) => message.role === "ASSISTANT" && !!message.fragment,
-    //     );
+    useEffect(() => {
+        const lastAssistantMessage = messages.findLast(
+            (message) => message.role === "ASSISTANT"
+        );
 
-    //     if (lastAssistantMessageWithFragment) {
-    //         setActiveFragment(lastAssistantMessageWithFragment.fragment);
-    //     }
-    // }, [messages, setActiveFragment]);
+        if (lastAssistantMessage?.fragment && lastAssistantMessage.id !== lastAssistantMessageIdRef.current) {
+            setActiveFragment(lastAssistantMessage.fragment);
+            lastAssistantMessageIdRef.current = lastAssistantMessage.id;
+        }
+    }, [messages, setActiveFragment]);
 
     useEffect(() => {
         bottomRef.current?.scrollIntoView();
